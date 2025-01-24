8a6d651c43fab5ecf08de4f5569931c8b6a332b9
Roeurnvanneth <roeurn.vanneth@institute.pse.ngo>
2025-01-24 11:35:37 +0700

    new code

---
+++ index.js
@@ -0,0 +1,31 @@
+let currentInput = "";
+
+function handleInput(value) {
+  const inputDisplay = document.getElementById("inputValue");
+
+  if (currentInput === "0" && value !== ".") {
+    currentInput = value; 
+  } else {
+    currentInput += value;
+  }
+
+  inputDisplay.textContent = currentInput; 
+}
+
+
+function handleReset() {
+  currentInput = "";
+  document.getElementById("inputValue").textContent = "0";
+}
+
+
+function handleEvaluate() {
+  try {
+    const result = Function(`'use strict'; return (${currentInput})`)();
+    document.getElementById("inputValue").textContent = result;
+    currentInput = result.toString();
+  } catch (error) {
+    document.getElementById("inputValue").textContent = "Error";
+    currentInput = "";
+  }
+}
